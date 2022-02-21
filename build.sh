199d6c0d70ffdd72100f73f6e206ff6b4611bc6f
Andrew Rasch <andrewarasch@gmail.com>
2022-02-20 20:53:07 -0500

    update build script

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eu
 
 repo_uri="https://x-access-token:${DEPLOY_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 remote_name="origin"
-main_branch="master"
+main_branch="main"
 target_branch="gh-pages"
 build_dir="dist"
 
